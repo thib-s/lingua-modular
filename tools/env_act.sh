851e7d2acd5475fcece1d5e16e5793371a07d274
thib-s <thibaut.boissin@irt-saintexupery.com>
2025-05-19 10:18:54 +0200

    added specific tooling

---
+++ tools/env_act.sh
@@ -0,0 +1,5 @@
+#module load arch/a100
+module purge
+module load miniforge/24.9.0      # lightweight Conda
+module load cuda/12.8.0
+conda activate lingua_25_250518
---
+++ tools/run_simple.sh
@@ -0,0 +1,1 @@
+torchrun --nproc_per_node=1 -m apps.main.train config=../apps/main/configs/llama_1B.yaml
