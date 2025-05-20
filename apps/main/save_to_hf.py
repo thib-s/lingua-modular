2a93d5e087ef0499447dbc54a8f206f8ced8ef49
Lucas Hervier <lucas.hervier@irt-saintexupery.com>
2025-05-20 15:32:56 +0000

    feat: save first locally before uploading models on HF hub

--- apps/main/save_to_hf.py
+++ apps/main/save_to_hf.py
@@ -5,29 +5,26 @@ from huggingface_hub import PyTorchModelHubMixin
 from typing import Optional
 from omegaconf import OmegaConf
 import torch
-from apps.main.generate import load_consolidated_model_and_tokenizer
+
 from apps.main.transformer import LMTransformer, LMTransformerArgs
-from lingua.args import dump_config
+from lingua.args import dataclass_from_dict, dump_config
 from lingua.checkpoint import CONSOLIDATE_FOLDER, consolidate_checkpoints
-# from lingua.distributed import (
-#     DistributedArgs,
-#     get_global_rank,
-#     setup_torch_distributed,
-# )
+from lingua.distributed import get_global_rank
 
 EVAL_FOLDER_NAME = "{:010d}"
+CONSOLIDATE_NAME = "consolidated.pth"
 
 logger = logging.getLogger()
 
 
 @dataclass
-class UploadArgs:
+class SaveHFArgs:
     username: str = ""
     repo_name: str = ""
-
     name: str = "upload"
     dump_dir: Optional[str] = None
     ckpt_dir: str = ""
+    base_hf_dir: str = ""
 
 
 class LinguaModelHub(torch.nn.Module,
@@ -35,33 +32,49 @@ class LinguaModelHub(torch.nn.Module,
     def __init__(self, model):
         super().__init__()
         self.model = model
-
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
 
+def load_consolidated_model(
+    consolidated_path,
+    model_cls=LMTransformer,
+    model_args_cls=LMTransformerArgs,
+):
+    ckpt_path = Path(consolidated_path)
+    config = ckpt_path / "params.json"
+    config = OmegaConf.load(config)
 
-def upload(cfg: UploadArgs):
-    # if not torch.distributed.is_initialized():
-    #     setup_torch_distributed(DistributedArgs())
-    # if (
-    #     Path(cfg.ckpt_dir).exists()
-    #     and (Path(cfg.ckpt_dir) / "params.json").exists()
-    #     and next(Path(cfg.ckpt_dir).glob("*.pth"), None) is not None
-    # ):
-    #     consolidate_path = Path(cfg.ckpt_dir)
-    # else:
-    #     consolidate_path = Path(cfg.ckpt_dir) / CONSOLIDATE_FOLDER
-    #     if not consolidate_path.exists() and get_global_rank() == 0:
-    #         consolidate_path = consolidate_checkpoints(cfg.ckpt_dir)
-    consolidate_path = Path(cfg.ckpt_dir) / CONSOLIDATE_FOLDER
+    param_dtype = dict(fp32=torch.float32, fp16=torch.float16, bf16=torch.bfloat16)[
+        config.distributed.model_dtype
+    ]
+    model_args = dataclass_from_dict(model_args_cls, config.model, strict=False)
+    model = model_cls(model_args)
+    st_dict = torch.load(ckpt_path / CONSOLIDATE_NAME, weights_only=True)
+    model.load_state_dict(st_dict["model"])
+    model = model.cuda().eval()
+    for param in model.parameters():
+        param.data = param.data.to(dtype=param_dtype)
+    return model, config
+
+def save_to_hf(cfg: SaveHFArgs):
+    if (
+        Path(cfg.ckpt_dir).exists()
+        and (Path(cfg.ckpt_dir) / "params.json").exists()
+        and next(Path(cfg.ckpt_dir).glob("*.pth"), None) is not None
+    ):
+        consolidate_path = Path(cfg.ckpt_dir)
+    else:
+        consolidate_path = Path(cfg.ckpt_dir) / CONSOLIDATE_FOLDER
+        if not consolidate_path.exists() and get_global_rank() == 0:
+            consolidate_path = consolidate_checkpoints(cfg.ckpt_dir)
 
     Path(cfg.dump_dir).mkdir(parents=True, exist_ok=True)
     dump_config(cfg, Path(cfg.dump_dir) / "config.yaml", log_config=False)
 
     consolidate_path = str(consolidate_path)
-    # torch.distributed.barrier()
+
     logger.info("Loading model")
-    model, tokenizer, train_cfg = load_consolidated_model_and_tokenizer(
+    model = load_consolidated_model(
         consolidate_path,
         model_cls=LMTransformer,
         model_args_cls=LMTransformerArgs,
@@ -69,8 +82,9 @@ def upload(cfg: UploadArgs):
     logger.info("Model loaded")
 
     lingua_model = LinguaModelHub(model)
-    lingua_model.push_to_hub(f"{cfg.username}/{cfg.repo_name}",
-                             private=True)
+    lingua_model.save_pretrained(f"{cfg.username}/{cfg.repo_name}")
+    # lingua_model.push_to_hub(f"{cfg.username}/{cfg.repo_name}",
+    #                          private=True)
 
 
 def main():
@@ -107,11 +121,10 @@ def main():
     # We remove 'config' attribute from config as the underlying DataClass does not have it
     del cli_args.config
 
-    default_cfg = OmegaConf.structured(UploadArgs())
+    default_cfg = OmegaConf.structured(SaveHFArgs())
     cfg = OmegaConf.merge(default_cfg, file_cfg, cli_args)
     cfg = OmegaConf.to_object(cfg)
-    upload(cfg)
-
+    save_to_hf(cfg)
 
 if __name__ == "__main__":
     main()
