cb096a0ab66c2dcc19bbb77e738f4409210811da
Lucas Hervier <lucas.hervier@irt-saintexupery.com>
2025-05-21 15:39:26 +0000

    feat: add slurm and train script lorasmollm

--- apps/main/lora_training.py
+++ apps/main/lora_training.py
@@ -1,9 +1,50 @@
 from transformers import Trainer, TrainingArguments
+from transformers import AutoTokenizer
 
-from peft import LoraConfig, TaskType
+from datasets import load_dataset
+
+from peft import LoraConfig
 from peft import get_peft_model
 
 from apps.main.save_to_hf import load_consolidated_model
+
+# Load the dataset
+# pubmed_path = "/lustre/fsmisc/dataset/HuggingFace/pubmed"
+# dataset = load_dataset(pubmed_path, split="train")
+# dataset = dataset.train_test_split(test_size=0.1)
+from datasets import load_dataset
+kwargs = dict(split="train")
+dataset = load_dataset("/lustre/fsmisc/dataset/HuggingFace/OpenLLM-France/Lucie-Training-Dataset", "Eurovoc", **kwargs)
+dataset = dataset.train_test_split(test_size=0.1)
+
+# Load the tokenizer
+tokenizer_path = "/lustre/fswork/projects/rech/reh/commun/tokenizers/llama3/original"
+
+# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B")
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+tokenizer.pad_token = tokenizer.eos_token
+
+# def preprocess(example):
+#     encoding = tokenizer(example["text"], truncation=True, padding="max_length", max_length=512)
+#     encoding["labels"] = encoding["input_ids"].copy()
+#     return encoding
+
+# tokenized_dataset = dataset.map(preprocess, batched=True, remove_columns=dataset["train"].column_names)
+
+def tokenize_batch_collate_fn(batch):
+    texts = [example["text"] for example in batch]
+    encodings = tokenizer(
+        texts,
+        padding="longest",  # or "max_length"
+        truncation=True,
+        max_length=512,
+        return_tensors="pt"
+    )
+    return {
+        "token_values": encodings["input_ids"],
+        "target": encodings["input_ids"].clone()
+    }
+
 model, model_cfg = load_consolidated_model("/lustre/fswork/projects/rech/reh/commun/dumps/modular_base/checkpoints/0000038000/consolidated")
 
 # peft_config = LoraConfig(task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1)
@@ -12,28 +53,33 @@ peft_config = LoraConfig(
 )
 model = get_peft_model(model, peft_config)
 
+output_dir = "/lustre/fswork/projects/rech/reh/commun/FOR-sight-ai/lingua-modular-eurovoc"
 training_args = TrainingArguments(
-    output_dir="/lustre/fswork/projects/rech/reh/commun/mt0-large-lora",
+    output_dir=output_dir,
     learning_rate=1e-3,
-    per_device_train_batch_size=32,
-    per_device_eval_batch_size=32,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
     num_train_epochs=2,
     weight_decay=0.01,
     eval_strategy="epoch",
     save_strategy="epoch",
     load_best_model_at_end=True,
+    remove_unused_columns=False,
 )
+
+def compute_loss_func(output):
+    return output
 
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["test"],
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics,
+    train_dataset=dataset['train'],
+    eval_dataset=dataset['test'],
+    processing_class=tokenizer,
+    data_collator=tokenize_batch_collate_fn,
+    compute_loss_func=compute_loss_func
 )
 
 trainer.train()
 
-model.save_pretrained("output_dir")
+model.save_pretrained(output_dir)
---
+++ tools/smollm_lora.py
@@ -0,0 +1,105 @@
+import os
+import math
+import argparse
+
+import torch
+
+from transformers import Trainer, TrainingArguments
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+from datasets import load_dataset
+
+from peft import LoraConfig, TaskType
+from peft import get_peft_model
+
+os.environ["WANDB_MODE"] = "offline"
+
+MODULE = {
+    "Pile-Freelaw": 14 * 1e9
+}
+
+def prepare_module_ds(module_name):
+    if module_name == "Pile-Freelaw":
+        kwargs = dict(split="train", streaming=True)
+        dataset = load_dataset("/lustre/fsmisc/dataset/HuggingFace/OpenLLM-France/Lucie-Training-Dataset", "Pile-FreeLaw", **kwargs)
+        return dataset
+    else:
+        raise ValueError(f"Unknown module name: {module_name}")
+
+def tokenize_batch_collate_fn(batch):
+    texts = [example["text"] for example in batch]
+    encodings = tokenizer(
+        texts,
+        padding="longest",         # or "max_length" if you prefer
+        truncation=True,
+        max_length=2048,
+        return_tensors="pt"
+    )
+    encodings["labels"] = encodings["input_ids"].clone()
+    return encodings
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--module_name", type=str, default="Pile-Freelaw")
+    argparser.add_argument("--nb_gpus", type=int, default=4)
+    argparser.add_argument("--batch_size", type=int, default=4)
+
+    args = argparser.parse_args()
+    NB_GPUS = args.nb_gpus
+    NB_TOKENS_MODULE = MODULE[args.module_name]
+    BATCH_SIZE = args.batch_size
+    MAX_STEPS = math.ceil(1.1 * NB_TOKENS_MODULE/ (NB_GPUS * 2048 * BATCH_SIZE))
+
+    output_dir = f"/lustre/fswork/projects/rech/reh/commun/FOR-sight-ai/SmolLM2-1.7B/{args.module_name}"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+
+    # Load the dataset
+    dataset = prepare_module_ds(args.module_name)
+
+    # Load the tokenizer
+    checkpoint = "/lustre/fswork/projects/rech/reh/commun/models/SmolLM2-1.7B"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    # Load the pre-trained model
+    model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto", torch_dtype=torch.bfloat16)
+
+    # LoRA preparation
+    peft_config = LoraConfig(
+        r=4,
+        lora_alpha=8,
+        target_modules=["q_proj", "v_proj"], # "k_proj", \"gate_proj\", \"up_proj\", \"down_proj\"\n",
+        lora_dropout=0.1,
+        bias="none",
+        task_type=TaskType.CAUSAL_LM
+    )
+    model = get_peft_model(model, peft_config)
+    # model.print_trainable_parameters()
+
+    # Training arguments
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        learning_rate=1e-3,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
+        num_train_epochs=1,
+        weight_decay=0.01,
+        eval_strategy="no",
+        save_strategy="steps",
+        logging_steps=1000,
+        save_steps=1000,
+        load_best_model_at_end=False,
+        remove_unused_columns=False,
+        bf16=True,
+        max_steps=MAX_STEPS
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset,
+        processing_class=tokenizer,
+        data_collator=tokenize_batch_collate_fn,
+    )
+    # Start training
+    trainer.train()
