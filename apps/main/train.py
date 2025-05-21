d0e1fd70d2ffbe6db70da507468f16271a92a124
Agustin Picard <agustin.picard@gmail.com>
2025-05-21 16:06:06 +0200

    train: clean-up

--- apps/main/train.py
+++ apps/main/train.py
@@ -258,15 +258,6 @@ def train(args: TrainArgs):
         # Initializing Model in meta device allows us to initialize models much bigger than 1 gpu's memory
         with torch.device("meta"):
             model = LMTransformer(args.model)
-            # if args.model.create_module_name != "None":
-            #     # freeze model parameters
-            #     for param in model.parameters():
-            #         param.requires_grad = False
-            #     # enable gradient for the new module
-            #     model.add_module(
-            #         args.model.create_module_name, args.model.module_seq_len
-            #     )
-            #     model.enable_module(args.model.create_module_name)
 
         logger.info("Model is built !")
 
